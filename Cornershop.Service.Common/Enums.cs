398b3463bd53b101fdc0ae78f4beac30385fd1fc
congbinh75 <dangcongbinh36@gmail.com>
2024-05-13 17:50:14 +0700

    Add Enums and TokenInfoProvider to Common

---
+++ Cornershop.Service.Common/Enums.cs
@@ -0,0 +1,12 @@
+﻿namespace Cornershop.Service.Common
+{
+    public static class Enums
+    {
+        public enum Role
+        {
+            Admin = 0,
+            Employee = 1,
+            Customer = 2
+        }
+    }
+}
---
+++ Cornershop.Service.Common/TokenInfoProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace Cornershop.Service.Common
+{
+    public interface ITokenInfoProvider
+    {
+        string? Id { get; }
+        string? Email { get; }
+        string? Role { get; }
+    }
+
+    public class TokenInfoProvider(IHttpContextAccessor httpContextAccessor) : ITokenInfoProvider
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+        public string? Id => _httpContextAccessor.HttpContext?.User?.Claims?.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        public string? Email => _httpContextAccessor.HttpContext?.User?.Claims?.FirstOrDefault(c => c.Type == ClaimTypes.Email)?.Value;
+        public string? Role => _httpContextAccessor.HttpContext?.User?.Claims?.FirstOrDefault(c => c.Type == ClaimTypes.Role)?.Value;
+    }
+}
