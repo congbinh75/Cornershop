ce23f1ce9d026390cb0572952a8ea1aa19380f39
congbinh75 <dangcongbinh36@gmail.com>
2024-05-13 11:33:13 +0700

    Initial commit

---
+++ Cornershop.Presentation.Consumer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Cornershop.Presentation.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cornershop - Rookie To Engineer Batch 7
+A project for Nashtech Rookie To Engineer Batch 7 by Binh Dang Cong.
