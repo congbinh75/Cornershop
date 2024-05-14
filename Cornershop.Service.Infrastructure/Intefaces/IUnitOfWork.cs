85655a6634412853941530fe077023258ffc3577
congbinh75 <dangcongbinh36@gmail.com>
2024-05-14 18:00:44 +0700

    Temporarily remove repositories and unit of work

---
+++ Cornershop.Service.Infrastructure/Intefaces/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+// namespace Cornershop.Service.Infrastructure.Interfaces
+// {
+//     public interface IUnitOfWork
+//     {
+//         Task SaveChanges();
+//         void Dispose();
+//     }
+// }
--- Cornershop.Service.Infrastructure/UnitOfWorks/UnitOfWork.cs
+++ Cornershop.Service.Infrastructure/UnitOfWorks/UnitOfWork.cs
@@ -1,33 +1,33 @@
-using Cornershop.Service.Infrastructure.Contexts;
-using Cornershop.Service.Repositories;
+// using Cornershop.Service.Infrastructure.Contexts;
+// using Cornershop.Service.Infrastructure.Interfaces;
 
-namespace Cornershop.Service.Infrastructure.UnitOfWorks
-{
-    public class UnitOfWork(CornershopDbContext context) : IUnitOfWork
-    {
-        private Dictionary<string, object> _repositories = [];
+// namespace Cornershop.Service.Infrastructure.UnitOfWorks
+// {
+//     public class UnitOfWork(CornershopDbContext context) : IUnitOfWork
+//     {
+//         private Dictionary<string, object> _repositories = [];
 
-        public IRepository<T> GetRepository<T>() where T : class
-		{
-			var type = typeof(T).Name;
+//         public IUserRepository<T> GetRepository<T>() where T : class
+// 		{
+// 			var type = typeof(T).Name;
 
-			if (_repositories.ContainsKey(type) == false)
-			{
-				var repositoryInstance = new Repository<T>(context);
-				_repositories.Add(type, repositoryInstance);
-			}
+// 			if (_repositories.ContainsKey(type) == false)
+// 			{
+// 				var repositoryInstance = new UserRepository<T>(context);
+// 				_repositories.Add(type, repositoryInstance);
+// 			}
 
-			return (IRepository<T>)_repositories[type];
-		}
+// 			return (IUserRepository<T>)_repositories[type];
+// 		}
 
-        public async Task SaveChanges()
-        {
-            await context.SaveChangesAsync();
-        }
+//         public async Task SaveChanges()
+//         {
+//             await context.SaveChangesAsync();
+//         }
 
-        public void Dispose()
-        {
-            context.Dispose();
-        }
-    }
-}
+//         public void Dispose()
+//         {
+//             context.Dispose();
+//         }
+//     }
+// }
