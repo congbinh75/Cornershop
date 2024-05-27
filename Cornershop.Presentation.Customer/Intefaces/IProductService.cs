ed46835099462ff37d6d29e78c3248928a39a7c4
congbinh75 <dangcongbinh36@gmail.com>
2024-05-27 23:20:50 +0700

    Add interfaces for services in Customer

---
+++ Cornershop.Presentation.Customer/Intefaces/IProductService.cs
@@ -0,0 +1,11 @@
+using Cornershop.Shared.DTOs;
+
+namespace Cornershop.Presentation.Customer.Intefaces;
+
+public interface IProductService
+{
+    public Task<ProductDTO?> GetById(string id, bool isHiddenIncluded = false);
+    public Task<ICollection<ProductDTO>> GetAll(int page, int pageSize, bool isHiddenIncluded = false);
+    public Task<ICollection<ProductDTO>> GetAllByCategory(string categoryId, int page, int pageSize, bool isHiddenIncluded = false);
+    public Task<ICollection<ProductDTO>> GetAllBySubcategory(string subcategoryId, int page, int pageSize, bool isHiddenIncluded = false);
+}
