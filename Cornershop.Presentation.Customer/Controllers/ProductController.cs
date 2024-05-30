7e48eea11eab170f977f18bdcc85108e54edc1b7
congbinh75 <dangcongbinh36@gmail.com>
2024-05-30 17:36:50 +0700

    Move from Category to Product for products gird in Customer

--- Cornershop.Presentation.Customer/Controllers/ProductController.cs
+++ Cornershop.Presentation.Customer/Controllers/ProductController.cs
@@ -1,11 +1,19 @@
+using Cornershop.Presentation.Customer.Intefaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Cornershop.Presentation.Controllers;
 
-public class ProductController : Controller
+public class ProductController(IProductService productService) : Controller
 {
-    public IActionResult Index()
+    public async Task<IActionResult> Index(int page = 1, int pageSize = 20, string? categoryId = null, string? subcategoryId = null)
     {
-        return View();
+        var products = await productService.GetAll(page, pageSize, categoryId, subcategoryId);
+        return View(products);
+    }
+
+    public async Task<IActionResult> Detail(string id)
+    {
+        var product = await productService.GetById(id);
+        return View(product);
     }
 }
