5e403ac519841a378383678a0027a5e3bff65f34
congbinh75 <dangcongbinh36@gmail.com>
2024-06-02 09:22:03 +0700

    Add CategoryFilter component in Customer

---
+++ Cornershop.Presentation.Customer/Controllers/Components/CategoryFilter.cs
@@ -0,0 +1,24 @@
+using Cornershop.Presentation.Customer.Intefaces;
+using Cornershop.Shared.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cornershop.Presentation.Customer.Components;
+
+public class CategoryFilterModel : PageModel
+{
+    public ICollection<CategoryDTO> Categories { get; set; } = [];
+
+    public ICollection<SubcategoryDTO> Subcategories { get; set; } = [];
+}
+
+public class CategoryFilter(ICategoryService categoryService, ISubcategoryService subcategoryService) : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(string? categoryId = null)
+    {
+        var categories = await categoryService.GetAll(1, 128);
+        ICollection<SubcategoryDTO> subcategories = [];
+        if (categoryId != null) subcategories = await subcategoryService.GetAllByCategory(categoryId, 1, 128);
+        return View(new CategoryFilterModel { Categories = categories, Subcategories = subcategories });
+    }
+}
